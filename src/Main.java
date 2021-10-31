9dba619e4bb0b6f7917a23548b6e57fa251ce800
Arsen Grigoryan <happysmilik@gmail.com>
2021-10-31 20:06:52 +0300

    code change

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int refill = 1200;
+        int refill = 500;
+        int balance = 100;
         int bonus = 0;
-        int score = 0;
+        int score = balance + refill;
 
         if (refill > 1000) {
             bonus = refill / 100;
-            score = refill + bonus;
+            score = score + bonus;
         }
+
         System.out.println("Сумма пополнения счета = " + refill + ". Баланс лицевого счета = " + score + ", в том числе бонусных рублей = " + bonus);
     }
 }
